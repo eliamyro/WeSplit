7f72f82982d410bf720ce22b051aca921c5ad780
eliamyro <elias.myronidis@hotmail.com>
2020-02-07 12:44:10 +0200

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Elias Myronidis on 7/2/20.
+//  Copyright © 2020 Elias Myronidis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
